aee0633f33181595fb8ebe5fa1dbf2899274d879
Andrew <andrew.shults@cgu.edu>
2020-05-21 22:14:41 -0700

    added a media folder and created a way to add thumbnails to the blog

--- blog/articles/models.py
+++ blog/articles/models.py
@@ -5,6 +5,7 @@ class Article(models.Model):
     slug = models.SlugField()
     body = models.TextField()
     date = models.DateTimeField(auto_now_add = True)
+    thumb = models.ImageField(default = 'default.png', blank=True)
     # add in thumbnail later
     #auther later
 
--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 from django.conf.urls import url, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,3 +14,5 @@ urlpatterns = [
 ]
 
 urlpatterns += staticfiles_urlpatterns()
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
